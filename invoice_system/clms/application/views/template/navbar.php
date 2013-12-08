19733aef40f4a33a04bd31123ec6cc1499a6c148
r3d7 <malikperang@gmail.com>
2013-12-08 16:09:39 +0800

    remove folder error on uploading

--- invoice_system/clms/application/views/template/navbar.php
+++
@@ -1,9 +0,0 @@
- <br /><br />
- <br /><br />
-         <ul class="navi">
-  <li><a href="<?php echo base_url();?>home/index"><i class="icon-home icon-white"></i> Form</a></li>
-  <li><a href="<?php echo base_url();?>database/index"><i class="icon-th-list icon-white"></i> Database</a></li>
-  <li style="float:right;"><a href="<?php echo base_url();?>login/logout"><i class="icon-off icon-white"></i> Logout</a></li>
-</ul>
-<br /><br />
-<div class="row-fluid">
